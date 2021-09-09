97d20494665bae157c76a8c73bf3a490c99c14a8
Giovanni Robira <grobira@twilio.com>
2021-09-09 14:46:50 -0300

    moving folders

--- flex-plugins/whisperCall/src/actions/index.js
+++ flex-plugins/whisperCall/src/actions/index.js
@@ -4,18 +4,14 @@ const handleWhisperTask = (payload) => {
 
     if (task.attributes.callType === 'whisper') {
         const workerSid = flex.Manager.getInstance().workerClient.sid;
-        console.log(workerSid)
-        console.log(task)
-        fetch(`https://a129-191-183-199-152.ngrok.io/taskAccepted?callSid=${task.attributes.callSid}&workerSid=${workerSid}&taskSid=${task.taskSid}`, { mode: 'cors' })
+        fetch(`https://twilio-function-6735-dev.twil.io/taskAccepted?callSid=${task.attributes.callSid}&workerSid=${workerSid}&taskSid=${task.taskSid}`, { mode: 'cors' })
             .then(res => res.text())
             .then(body => console.log(body))
     }
 }
 
 const registerActions = (flex) => {
-
     flex.Actions.addListener("beforeAcceptTask", handleWhisperTask)
-
 }
 
 module.exports = { registerActions }
